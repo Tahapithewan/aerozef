21749f25d363897c9ef672ab6509bfc32aeff2f6
Tahapithewan <tahapithewan53@gmail.com>
2023-06-07 22:09:26 +0530

    Student front-end and back-end Processed

--- src/modules/user/components/pages/Student/Student.jsx
+++ src/modules/user/components/pages/Student/Student.jsx
@@ -1,44 +1,38 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React,{useEffect ,useState} from 'react'
 import { useFormik } from 'formik'
-import Navbar from '../../shared/Navbar/Navbar'
-import axios from "axios"
-import { InsertData } from '../../../../../Services/ApplicantService'
-
-const Applicants = () => {
+import { useNavigate } from 'react-router-dom'
+import {fetchData , InsertData} from '../../../../../Services/StudentServices'
+const Student = () => {
     let navigate = useNavigate()
-    let FormValues = {
-        image : "",
-        heading : "",
-        description : "" ,
-    }
-    let {handleSubmit , handleChange ,touched ,errors , setFieldValue} = useFormik({
-        initialValues : FormValues,
-        onSubmit : async(data)=>{
-            let form = new FormData()
-            for (var properties in data){
-                form.append(properties , data[properties])
-            }        
+    let {handleChange , handleSubmit ,errors , values ,setFieldValue} = useFormik({
+        initialValues : {image : "" , heading : "" , description : ""},
+        onSubmit : async (data)=>{
+            console.log(data)
+            let form = new FormData();
+            for (let value in data){
+                form.append(value, data[value])
+            }
             let response = await InsertData(form)
-            // console.log(res.data)
-            navigate("../profile")
+            // console.log(response)
+            navigate("../studentProfile")
+
+            
         }
     })
-  return (
+  return (  
     <>
-    <Navbar />
     <div className="container my-5">
         <form onSubmit={handleSubmit}>
         <div className="row">
             <div className="col-md-6 offset-md-3">
                 <div className="card">
                     <div className="card-header bg-dark">
-                        <h2 className='text-light text-center my-3'>Applicant Form</h2>
+                        <h2 className='text-light text-center my-3'>Student Form</h2>
                     </div>
                     <div className="card-body bg-light">
                         <div className="form-group">
                             <label htmlFor="IM" className='mb-3'><h4>Insert Image :</h4></label>
-                            <input type="file" name="image" id="IM" className='form-control' onChange={(e)=>setFieldValue("image", e.target.files[0])}/>
+                            <input type="file" name="image" id="IM" className='form-control' onChange={(e)=>setFieldValue("image",e.target.files[0])}/>
                         </div>
                         <div className="form-group my-5">
                             <label htmlFor="HE" className='mb-3'><h4>Heading :</h4></label>
@@ -62,4 +56,4 @@ const Applicants = () => {
   )
 }
 
-export default Applicants
+export default Student
